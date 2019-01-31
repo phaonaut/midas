0d0ef101ee0d24fd7f5eb5ba011326c15b3062a1
Rodney <phaonaut@gmail.com>
2019-01-30 21:33:41 -0600

    add material, icons, lab and p5 to package and  p5 wrapper and basic sketch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import P5Wrapper from "./features/util/P5Wrapper";
+import sketch from "./features/sketch/sketch";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <P5Wrapper sketch={sketch} />
       </div>
     );
   }
---
+++ src/features/sketch/sketch.js
@@ -0,0 +1,36 @@
+export default function sketch(p) {
+    // let rotation = 0;
+    var fillColor = "#4286f4"; //staring color
+    var strokeColor = "#4286f4"; //staring stroke color
+    var brushSize = 40; //starting brush size
+  
+    p.setup = function() {
+      p.createCanvas(600, 400, p.P2D);
+      p.background('white');
+    };
+  
+    // p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
+    //   if (props.rotation) {
+    //     rotation = (props.rotation * Math.PI) / 180;
+    //   }
+    // };
+  
+    p.draw = function() {
+      console.log("!! mouse", p.mouseX, p.mouseY);
+      if (p.mouseIsPressed) {
+        p.fill(fillColor);
+        p.ellipse(p.mouseX, p.mouseY, brushSize, brushSize, 50);
+        p.stroke(strokeColor);
+      } else {
+        p.noFill();
+        p.noStroke();
+      }
+      // p.background(100);
+      // p.noStroke();
+      // p.push();
+      // p.rotateY(rotation);
+      // p.box(100);
+      // p.pop();
+    };
+  }
+  
