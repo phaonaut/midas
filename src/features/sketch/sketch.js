2ea1e3ff50e5246aeb04e049e1f2802bb681ac8b
Rodney <phaonaut@gmail.com>
2019-12-24 08:52:18 -0600

    updated versions
    updated to latest versions of material and react, added predefined steps to slider

--- src/features/sketch/sketch.js
+++ src/features/sketch/sketch.js
@@ -1,39 +1,39 @@
 export default function sketch(p) {
-    var fillColor = "#4286f4"; //staring color
-    // var strokeColor = "#4286f4"; //staring stroke color
-    var brushRadius = 40; //starting brush size
-  
-    p.setup = function() {
-      p.createCanvas(500, 400, p.P2D);
-      p.background('white');
-    };
-  
-    p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
-      if(props.saveImage) {
-        p.saveCanvas("mydrawing", "jpg");
-        props.resetStatetoDefault("saveImage");
-      }
-      if(props.clearCanvas) {
-        p.background('white');
-        props.resetStatetoDefault("clearCanvas");
-      }
-      if(props.brush) {
-        brushRadius = props.brush.radius;
-        fillColor = props.brush.fillColor;
-      }
-    };
-  
-    p.draw = function() {
-      if (p.mouseIsPressed) {
-        p.fill(fillColor);
-        p.ellipse(p.mouseX, p.mouseY, brushRadius, brushRadius, 50);
-        // p.rect(p.mouseX, p.mouseY, brushRadius, brushRadius);
-        // p.stroke(strokeColor);
-        // p.filter( p.BLUR, 6 ); //slow as fudge
-      } else {
-        p.noFill();
-        p.noStroke();
-      }
-    };
-  }
-  
+  var fillColor = "#4286f4"; //staring color
+  // var strokeColor = "#4286f4"; //staring stroke color
+  var brushRadius = 40; //starting brush size
+
+  p.setup = function() {
+    p.createCanvas(500, 400, p.P2D);
+    p.background("white");
+  };
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
+    if (props.saveImage) {
+      p.saveCanvas("mydrawing", "jpg");
+      props.resetStatetoDefault("saveImage");
+    }
+    if (props.clearCanvas) {
+      p.background("white");
+      props.resetStatetoDefault("clearCanvas");
+    }
+    if (props.brush) {
+      brushRadius = props.brush.radius;
+      fillColor = props.brush.fillColor;
+    }
+  };
+
+  p.draw = function() {
+    if (p.mouseIsPressed) {
+      // console.log(p.frameRate());
+      p.fill(fillColor);
+      p.ellipse(p.mouseX, p.mouseY, brushRadius, brushRadius, 50);
+      // p.rect(p.mouseX, p.mouseY, brushRadius, brushRadius);
+      // p.stroke(strokeColor);
+      // p.filter( p.BLUR, 6 ); //slow as fudge
+    } else {
+      p.noFill();
+      p.noStroke();
+    }
+  };
+}
--- src/features/toolbars/brushToolbar.js
+++ src/features/toolbars/brushToolbar.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Typography, withStyles } from '@material-ui/core';
-import { Slider } from '@material-ui/lab';
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography, withStyles } from "@material-ui/core";
+import { Slider } from "@material-ui/core";
 
 const styles = theme => ({
   container: {
     width: 300,
     padding: `${theme.spacing.unit}px ${theme.spacing.unit}px`,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
     flexDirection: "column",
     margin: `${theme.spacing.unit / 2}px`,
-    background: theme.palette.background.default,
+    background: theme.palette.background.default
   },
   sliderContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
     width: "100%"
   },
   slider: {
     margin: "0 5px",
-    padding: '12px 0px',
-  },
+    padding: "12px 0px"
+  }
 });
 
 class BrushToolbar extends React.Component {
   state = {
     brushValue: 50,
-    strokeValue: 0,
+    strokeValue: 0
   };
 
   handleBrushChange = (event, brushValue) => {
@@ -49,21 +49,12 @@ class BrushToolbar extends React.Component {
       <div className={classes.container}>
         <div className={classes.sliderContainer}>
           <Typography>Radius</Typography>
-          <Slider
-            classes={{ container: classes.slider }}
-            value={brushValue}
-            onChange={this.handleBrushChange}
-            />
+          <Slider step={25} marks min={0} max={100} classes={{ root: classes.slider }} value={brushValue} onChange={this.handleBrushChange} />
           <Typography>{Math.round(brushValue)}</Typography>
         </div>
         <div className={classes.sliderContainer}>
           <Typography>Stroke</Typography>
-          <Slider
-            classes={{ container: classes.slider }}
-            value={strokeValue}
-            onChange={this.handleStrokeChange}
-            disabled
-            />
+          <Slider classes={{ root: classes.slider }} value={strokeValue} onChange={this.handleStrokeChange} disabled />
           <Typography>{Math.round(strokeValue)}</Typography>
         </div>
       </div>
@@ -74,7 +65,7 @@ class BrushToolbar extends React.Component {
 BrushToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
   brushRadius: PropTypes.number.isRequired,
-  onUpdateBrushRadius: PropTypes.func.isRequired,
+  onUpdateBrushRadius: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(BrushToolbar);
